2daa26792cd890867d4e130a4978af808fee35af
graykode <nlkey2022@gmail.com>
2025-04-18 21:07:51 +0900

    feat: change index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import { useRouter } from 'next/router'
+import { monadTestnet } from 'viem/chains'
+
+import { useChainContext } from '../contexts/chain-context'
 
 export default function Home() {
   const router = useRouter()
+  const { selectedChain } = useChainContext()
   if (router.pathname === '/') {
-    router.push('/trade')
+    if (selectedChain.id === monadTestnet.id) {
+      router.push('/trading-competition')
+    } else {
+      router.push('/trade')
+    }
   }
   return <></>
 }
