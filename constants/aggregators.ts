3c784f0e85b5c1dbbb91d5e3024f101d8292d890
graykode <nlkey2022@gmail.com>
2025-04-19 13:56:43 +0900

    feat: change open ocean aggregator address

--- constants/aggregators.ts
+++ constants/aggregators.ts
@@ -28,7 +28,8 @@ export const AGGREGATORS: {
       monadTestnet,
     ),
     new OpenOceanAggregator(
-      getAddress('0xfD845859628946B317A78A9250DA251114FbD846'), // aggregator wrapper
+      // getAddress('0xfD845859628946B317A78A9250DA251114FbD846'), // aggregator wrapper
+      getAddress('0x6352a56caadC4F1E25CD6c75970Fa768A3304e64'),
       monadTestnet,
     ),
   ],
