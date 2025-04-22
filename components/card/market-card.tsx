67cb7a2880d1e32959636fcab8cf52ac6121820c
graykode <nlkey2022@gmail.com>
2025-04-22 16:35:22 +0900

    fix: fix css

--- components/card/market-card.tsx
+++ components/card/market-card.tsx
@@ -69,7 +69,7 @@ export const MarketCard = ({
             `/trade?inputCurrency=${baseCurrency.address}&outputCurrency=${quoteCurrency.address}`,
           )
         }}
-        className={`transition-colors duration-500 ${flashState === 'green' ? 'bg-[#39e79f]/30' : flashState === 'red' ? 'bg-red-500/30' : 'bg-gray-800'} hidden lg:flex max-w-[1072px] text-left h-16 px-5 py-4 hover:bg-gray-700 rounded-2xl justify-start items-center gap-4`}
+        className={`transition-colors duration-500 ${flashState === 'green' ? 'bg-[#39e79f]/30' : flashState === 'red' ? 'bg-red-500/30' : 'bg-gray-800'} hidden lg:flex max-w-[1072px] text-left h-16 px-5 py-4 rounded-2xl justify-start items-center gap-4`}
       >
         <div className="flex w-[280px] items-center gap-3">
           <div className="w-14 h-8 shrink-0 relative">
