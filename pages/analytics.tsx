c108d43eaa4b618f2a35d8542f30214b77cdd72f
graykode <nlkey2022@gmail.com>
2025-04-24 14:43:18 +0900

    feat: add retention map

--- pages/analytics.tsx
+++ pages/analytics.tsx
@@ -3,12 +3,17 @@ import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 import { UTCTimestamp } from 'lightweight-charts'
 import { getAddress, isAddressEqual } from 'viem'
+import { TamaguiProvider } from '@tamagui/web'
 
 import { HistogramChart } from '../components/chart/histogram-chart'
 import { useChainContext } from '../contexts/chain-context'
 import { useCurrencyContext } from '../contexts/currency-context'
 import RedirectIfNotMonadTestnetContainer from '../containers/redirect-if-not-monad-testnet-container'
 import { Loading } from '../components/loading'
+import tamaguiConfig from '../tamagui.config'
+import { Chart } from '../components/chart/chart-model'
+import { TVLChartModel } from '../components/chart/tvl-chart-model'
+import { ChartHeader } from '../components/chart/chart-header'
 
 export default function Analytics() {
   const { prices, whitelistCurrencies } = useCurrencyContext()
@@ -43,6 +48,24 @@ export default function Analytics() {
     },
     initialData: [],
   })
+
+  const retentionRate = useMemo(
+    () =>
+      [
+        { date: '2025-03-09', value: 100 },
+        { date: '2025-03-16', value: 17.2 },
+        { date: '2025-03-23', value: 13.9 },
+        { date: '2025-03-30', value: 11.7 },
+        { date: '2025-04-06', value: 12.7 },
+        { date: '2025-04-13', value: 13.0 },
+      ].map((item) => ({
+        time: Math.floor(
+          new Date(item.date).getTime() / 1000,
+        ) as unknown as UTCTimestamp,
+        values: [item.value, 0],
+      })),
+    [],
+  )
 
   const tokenColorMap = useMemo(() => {
     return Object.fromEntries(
@@ -178,6 +201,75 @@ export default function Analytics() {
                   height={312}
                 />
               </div>
+            </div>
+          </div>
+
+          <div className="flex flex-col sm:flex-row gap-4">
+            <div className="flex flex-col flex-1">
+              <div className="text-white text-sm md:text-base font-bold">
+                Retention Rate
+              </div>
+
+              <div className="flex w-[350px] sm:w-[500px]">
+                <TamaguiProvider
+                  config={tamaguiConfig}
+                  disableInjectCSS
+                  disableRootThemeClass
+                >
+                  {(() => {
+                    return (
+                      <Chart
+                        Model={TVLChartModel}
+                        params={
+                          {
+                            data: retentionRate,
+                            colors: ['#4C82FB', '#FC72FF'],
+                            gradients: [
+                              {
+                                start: 'rgba(96, 123, 238, 0.20)',
+                                end: 'rgba(55, 70, 136, 0.00)',
+                              },
+                              {
+                                start: 'rgba(252, 116, 254, 0.20)',
+                                end: 'rgba(252, 116, 254, 0.00)',
+                              },
+                            ],
+                          } as any
+                        }
+                        height={368}
+                      >
+                        {(crosshairData) => {
+                          const value = crosshairData
+                            ? crosshairData.values[0]
+                            : (retentionRate[retentionRate.length - 1] as any)
+                                .values[0]
+                          return (
+                            <ChartHeader
+                              value={`${value.toFixed(2)}%`}
+                              time={crosshairData?.time as any}
+                              detailData={[
+                                {
+                                  label: 'rate',
+                                  color: '#FC72FF',
+                                  value: `${(
+                                    (crosshairData as any)?.values[0] ?? 0
+                                  ).toFixed(2)}%`,
+                                },
+                              ]}
+                            />
+                          )
+                        }}
+                      </Chart>
+                    )
+                  })()}
+                </TamaguiProvider>
+              </div>
+            </div>
+
+            <div className="flex flex-col flex-1">
+              <div className="text-white text-sm md:text-base font-bold"></div>
+
+              <div className="flex w-[350px] sm:w-[500px]"></div>
             </div>
           </div>
         </div>
