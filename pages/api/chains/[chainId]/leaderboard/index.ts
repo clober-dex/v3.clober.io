db028cb6c814000c7292ed1b2c0e7a2b41c45937
graykode <nlkey2022@gmail.com>
2025-04-30 13:46:48 +0900

    feat: show user's 100 to 1000

--- pages/api/chains/[chainId]/leaderboard/index.ts
+++ pages/api/chains/[chainId]/leaderboard/index.ts
@@ -30,7 +30,7 @@ export default async function handler(
       return
     }
 
-    const sql = `SELECT encode(t.origin, 'hex') AS wallet, SUM( ( (t.input_amount / POWER(10, COALESCE(meta_i.decimals, 18))) * COALESCE(meta_i.price, 0) + (t.output_amount / POWER(10, COALESCE(meta_o.decimals, 18))) * COALESCE(meta_o.price, 0) ) / 2 ) AS total_volume_usd FROM trade t LEFT JOIN token_metadata meta_i ON t.input_token = meta_i.token AND meta_i.chain_id = $1 LEFT JOIN token_metadata meta_o ON t.output_token = meta_o.token AND meta_o.chain_id = $1 GROUP BY t.origin ORDER BY total_volume_usd DESC LIMIT 100`
+    const sql = `SELECT encode(t.origin, 'hex') AS wallet, SUM( ( (t.input_amount / POWER(10, COALESCE(meta_i.decimals, 18))) * COALESCE(meta_i.price, 0) + (t.output_amount / POWER(10, COALESCE(meta_o.decimals, 18))) * COALESCE(meta_o.price, 0) ) / 2 ) AS total_volume_usd FROM trade t LEFT JOIN token_metadata meta_i ON t.input_token = meta_i.token AND meta_i.chain_id = $1 LEFT JOIN token_metadata meta_o ON t.output_token = meta_o.token AND meta_o.chain_id = $1 GROUP BY t.origin ORDER BY total_volume_usd DESC LIMIT 1000`
     const { rows } = await query(sql, [chainId])
 
     return res.status(200).json({
