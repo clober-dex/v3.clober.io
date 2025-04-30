8d2e80366e97d8e6ff7fb76006b23da79a548932
graykode <nlkey2022@gmail.com>
2025-04-30 18:18:31 +0900

    fix: fix css

--- components/button/user-point-button.tsx
+++ components/button/user-point-button.tsx
@@ -9,7 +9,7 @@ export const UserPointButton = ({ router }: { router: NextRouter }) => {
       onClick={() => {
         router.push('/leaderboard')
       }}
-      className="group relative cursor-pointer w-full flex h-8 p-2 sm:px-3 text-sm lg:text-base justify-end bg-gray-700 text-gray-50 hover:bg-gray-500 items-center gap-1 shrink-0 border-solid rounded"
+      className="group font-semibold relative cursor-pointer w-full flex h-8 p-2 sm:px-3 text-sm lg:text-base justify-end bg-gray-700 text-gray-50 hover:bg-gray-500 items-center gap-1 shrink-0 border-solid rounded"
     >
       <span className="flex">Points</span>
 
