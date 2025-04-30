80de675bb006958550fdb72cf88fe1071ac1e449
graykode <nlkey2022@gmail.com>
2025-04-30 10:37:10 +0900

    feat: set default page as trade

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,10 @@
 import React from 'react'
 import { useRouter } from 'next/router'
-import { monadTestnet } from 'viem/chains'
-
-import { useChainContext } from '../contexts/chain-context'
 
 export default function Home() {
   const router = useRouter()
-  const { selectedChain } = useChainContext()
   if (router.pathname === '/') {
-    if (selectedChain.id === monadTestnet.id) {
-      router.push('/trading-competition')
-    } else {
-      router.push('/trade')
-    }
+    router.push('/trade')
   }
   return <></>
 }
