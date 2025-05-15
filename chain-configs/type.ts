3407a14d9fcf9e9e6e7f4b2fca4778e5e4b6b06b
graykode <nlkey2022@gmail.com>
2025-05-15 17:20:09 +0900

    refactor: update ChainConfig type to use Record for external contract addresses and subgraph endpoints

--- chain-configs/type.ts
+++ chain-configs/type.ts
@@ -1,17 +1,20 @@
 import { Chain } from '../model/chain'
 import { Currency } from '../model/currency'
 
+type EXTERNAL_CONTRACT =
+  | 'FuturesMarket'
+  | 'PythOracle'
+  | 'TradingCompetitionRegistration'
+
+type EXTERNAL_SUBGRAPH = 'FUTURES' | 'LIQUIDITY_VAULT_POINT'
+
 export type ChainConfig = {
   CHAIN: Chain
   GOOGLE_ANALYTICS_TRACKING_ID: string
   RPC_URL: string
   PYTH_HERMES_ENDPOINT: string
-  EXTERNAL_CONTRACT_ADDRESSES: {
-    [contractName: string]: `0x${string}`
-  }
-  EXTERNAL_SUBGRAPH_ENDPOINTS: {
-    [endpointName: string]: string
-  }
+  EXTERNAL_CONTRACT_ADDRESSES: Record<EXTERNAL_CONTRACT, `0x${string}`>
+  EXTERNAL_SUBGRAPH_ENDPOINTS: Record<EXTERNAL_SUBGRAPH, `https://${string}`>
   WHITELISTED_VAULT_KEYS: `0x${string}`[]
   REFERENCE_CURRENCY: Currency
   DEFAULT_INPUT_CURRENCY: Currency
