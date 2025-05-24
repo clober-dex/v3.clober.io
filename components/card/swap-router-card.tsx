4cbb867e8101288e8d66dcc459b0b628839e26e6
graykode <nlkey2022@gmail.com>
2025-05-24 22:49:22 +0900

    fix: disable quote selection button when no quote is set

--- components/card/swap-router-card.tsx
+++ components/card/swap-router-card.tsx
@@ -28,6 +28,7 @@ export const SwapRouteCard = ({
       onClick={
         setSelectedQuote ? () => setSelectedQuote(quote ?? null) : undefined
       }
+      disabled={!setSelectedQuote}
       className={`hover:bg-[#e5eaff]/10 h-full sm:h-[84px] text-white w-full self-stretch px-3.5 sm:px-4 py-3 bg-[#e5eaff]/5 rounded-xl flex flex-col justify-start items-start gap-1.5 sm:gap-3 ${isSelected ? 'outline outline-[1.20px] outline-offset-[-1.20px] outline-blue-400/80' : ''}`}
     >
       <div className="self-stretch flex justify-start items-start gap-1">
