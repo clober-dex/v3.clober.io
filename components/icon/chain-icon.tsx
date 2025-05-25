73bc645ab7b9cf308921c0baae786f921507dd52
graykode <nlkey2022@gmail.com>
2025-05-25 11:16:46 +0900

    fix: enhance ChainIcon component to use default icon from CHAIN_CONFIG

--- components/icon/chain-icon.tsx
+++ components/icon/chain-icon.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { Chain } from '../../model/chain'
+import { CHAIN_CONFIG } from '../../chain-configs'
 
 export default function ChainIcon({
   chain,
@@ -9,10 +10,16 @@ export default function ChainIcon({
   chain: Chain
 }) {
   const name = chain.name.toLowerCase().split(' ')[0]
+  const defaultIcon = CHAIN_CONFIG.CHAIN.icon
   return (
     <div {...props}>
       <img
         src={chain.icon || `https://assets.odos.xyz/chains/${name}.png`}
+        onError={(e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+          e.currentTarget.onerror = null
+          // @ts-ignore
+          e.currentTarget.src = defaultIcon
+        }}
         alt="ChainIcon"
         className="w-full h-full object-contain rounded-full"
       />
